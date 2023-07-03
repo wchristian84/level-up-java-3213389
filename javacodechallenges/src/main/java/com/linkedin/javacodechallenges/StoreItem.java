83ef5023ddef2c36c0432a1b92536b264157070a
wchristian84 <99511387+wchristian84@users.noreply.github.com>
2023-07-03 10:28:52 -0500

    Update StoreItem.java
    manually copying over changes from codespace due to error when pushing changes

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/StoreItem.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/StoreItem.java
@@ -1,6 +1,7 @@
 package com.linkedin.javacodechallenges;
 
 import java.util.Collection;
+import java.util.ArrayList;
 import java.util.Optional;
 
 import lombok.AllArgsConstructor;
@@ -16,8 +17,30 @@ public class StoreItem {
   double discount;
 
   public static Optional<StoreItem> findLeastExpensive(Collection<StoreItem> items) {
-    // TODO: Implement
-    return Optional.empty();
+    if (items.size() == 0) {
+      return Optional.empty();
+    }
+    else {
+      StoreItem returnItem;
+      ArrayList<StoreItem> itemList = new ArrayList<StoreItem>();
+      ArrayList<Double> prices = new ArrayList<Double>();
+
+      items.stream().forEach(item -> {
+        itemList.add(item);
+        prices.add(item.retailPrice * (1.0 - item.discount));
+      });
+
+      int min = 0;
+      for (int i = 1; i < prices.size(); i++) {
+        if (prices.get(i) < prices.get(min)) {
+          min = i;
+        }
+      }
+      returnItem = itemList.get(min);
+      
+
+      return Optional.of(returnItem);
+    }
   }
 
   @Override
