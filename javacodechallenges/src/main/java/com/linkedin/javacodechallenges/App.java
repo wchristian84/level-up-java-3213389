d54c21dbf22eff9cc6d98a379799054a3a182b69
wchristian84 <99511387+wchristian84@users.noreply.github.com>
2023-07-03 10:23:10 -0500

    Update App.java
    changes wouldn't push from codespace, so I'm manually copying them over here

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -5,7 +5,34 @@ import java.util.Scanner;
 public class App {
 
     public static boolean isPasswordComplex(String password) {
-        return false;
+        boolean upper = false;
+        boolean lower = false;
+        boolean num = false;
+
+        if (password.length() < 6){
+            return false;
+        }
+
+        for (int i = 0; i < password.length(); i++) {
+            char passChar = password.charAt(i); 
+            if (passChar >= 'A' && passChar <= 'Z'){
+                upper = true;
+            }
+
+            if (passChar >= 'a' && passChar <= 'z'){
+                lower = true;
+            }
+
+            if (passChar >= '0' && passChar <= '9'){
+                num = true;
+            }
+        }
+        if (num && lower && upper){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     public static void main(String[] args) {
