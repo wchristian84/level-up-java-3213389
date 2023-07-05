bfb9420fe9dbadcc05656b90889b09804802fe95
wchristian84 <wade.christian@gmail.com>
2023-07-05 12:00:00 -0500

    simplified loop

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/TeamUtils.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/TeamUtils.java
@@ -29,29 +29,54 @@ public class TeamUtils {
 
       teamArray.sort(Comparator.comparing(Team::sumTotalScore).reversed());
 
-      System.out.println("Now for the results, the WINNER is...\n");
-      if (teamArray.get(0).sumTotalScore() == teamArray.get(1).sumTotalScore()) {
-        System.out.println("It's a TIE!\n");
-        System.out.println("With " + teamArray.get(0).sumTotalScore() +
-            " points, it's team " + teamArray.get(0).getPlayer1() + " and " + teamArray.get(0).getPlayer2() + "!\n");
-        System.out.println("With " + teamArray.get(1).sumTotalScore() +
-            " points, it's team " + teamArray.get(1).getPlayer1() + " and " + teamArray.get(1).getPlayer2() + "!\n");
-        for (int i = 2; i < teamArray.size(); i++) {
-          System.out.println("Then we have... \n" +
+      System.out.println("Now for the results, the WINNER is...");
+
+      for (int i = 0; i < teamArray.size(); i++) {
+        if (teamArray.get(i).sumTotalScore() == teamArray.get(i + 1).sumTotalScore()) {
+          System.out.println(
               "With " + teamArray.get(i).sumTotalScore() + " points, it's team " + teamArray.get(i).getPlayer1()
-              + " and "
-              + teamArray.get(i).getPlayer2() + "!\n");
+                  + " and " + teamArray.get(i).getPlayer2() + "!\n");
+          System.out.println(
+              "With " + teamArray.get(i + 1).sumTotalScore() + " points, it's team " + teamArray.get(i + 1).getPlayer1()
+                  + " and " + teamArray.get(i + 1).getPlayer2() + "!\n");
+          i++;
+        } else {
+          System.out.println(
+              "With " + teamArray.get(i).sumTotalScore() + " points, it's team " + teamArray.get(i).getPlayer1()
+                  + " and " + teamArray.get(i).getPlayer2() + "!\n");
         }
-      } else {
-        System.out.println("With " + teamArray.get(0).sumTotalScore() +
-            " points, it's team " + teamArray.get(0).getPlayer1() + " and " + teamArray.get(0).getPlayer2() + "!\n");
-        for (int i = 2; i < teamArray.size(); i++) {
-          System.out.println("Then we have... \n" +
-              "With " + teamArray.get(i).sumTotalScore() + " points, it's team " + teamArray.get(i).getPlayer1()
-              + " and "
-              + teamArray.get(i).getPlayer2() + "!\n");
+        if (i < (teamArray.size() - 1)) {
+          System.out.println("Then we have... \n");
         }
       }
+
+      // if (teamArray.get(0).sumTotalScore() == teamArray.get(1).sumTotalScore()) {
+      // System.out.println("It's a TIE!\n");
+      // System.out.println("With " + teamArray.get(0).sumTotalScore() +
+      // " points, it's team " + teamArray.get(0).getPlayer1() + " and " +
+      // teamArray.get(0).getPlayer2() + "!\n");
+      // System.out.println("With " + teamArray.get(1).sumTotalScore() +
+      // " points, it's team " + teamArray.get(1).getPlayer1() + " and " +
+      // teamArray.get(1).getPlayer2() + "!\n");
+      // for (int i = 2; i < teamArray.size(); i++) {
+      // System.out.println("Then we have... \n" +
+      // "With " + teamArray.get(i).sumTotalScore() + " points, it's team " +
+      // teamArray.get(i).getPlayer1()
+      // + " and "
+      // + teamArray.get(i).getPlayer2() + "!\n");
+      // }
+      // } else {
+      // System.out.println("With " + teamArray.get(0).sumTotalScore() +
+      // " points, it's team " + teamArray.get(0).getPlayer1() + " and " +
+      // teamArray.get(0).getPlayer2() + "!\n");
+      // for (int i = 1; i < teamArray.size(); i++) {
+      // System.out.println("Then we have... \n" +
+      // "With " + teamArray.get(i).sumTotalScore() + " points, it's team " +
+      // teamArray.get(i).getPlayer1()
+      // + " and "
+      // + teamArray.get(i).getPlayer2() + "!\n");
+      // }
+      // }
     }
 
   }
