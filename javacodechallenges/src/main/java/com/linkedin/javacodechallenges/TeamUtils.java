4b875cf2fe1c66007cd595c44f09a3b0b3d4ee64
wchristian84 <wade.christian@gmail.com>
2023-07-05 11:37:01 -0500

    initial commit

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/TeamUtils.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/TeamUtils.java
@@ -2,6 +2,9 @@ package com.linkedin.javacodechallenges;
 
 import java.util.List;
 import java.util.Random;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class TeamUtils {
 
@@ -16,6 +19,40 @@ public class TeamUtils {
   }
 
   public static void revealResults(List<Team> teams) {
+    if (teams.size() == 0) {
+      return;
+    } else {
+      ArrayList<Team> teamArray = new ArrayList<Team>();
+      for (int i = 0; i < teams.size(); i++) {
+        teamArray.add(teams.get(i));
+      }
+
+      teamArray.sort(Comparator.comparing(Team::sumTotalScore).reversed());
+
+      System.out.println("Now for the results, the WINNER is...\n");
+      if (teamArray.get(0).sumTotalScore() == teamArray.get(1).sumTotalScore()) {
+        System.out.println("It's a TIE!\n");
+        System.out.println("With " + teamArray.get(0).sumTotalScore() +
+            " points, it's team " + teamArray.get(0).getPlayer1() + " and " + teamArray.get(0).getPlayer2() + "!\n");
+        System.out.println("With " + teamArray.get(1).sumTotalScore() +
+            " points, it's team " + teamArray.get(1).getPlayer1() + " and " + teamArray.get(1).getPlayer2() + "!\n");
+        for (int i = 2; i < teamArray.size(); i++) {
+          System.out.println("Then we have... \n" +
+              "With " + teamArray.get(i).sumTotalScore() + " points, it's team " + teamArray.get(i).getPlayer1()
+              + " and "
+              + teamArray.get(i).getPlayer2() + "!\n");
+        }
+      } else {
+        System.out.println("With " + teamArray.get(0).sumTotalScore() +
+            " points, it's team " + teamArray.get(0).getPlayer1() + " and " + teamArray.get(0).getPlayer2() + "!\n");
+        for (int i = 2; i < teamArray.size(); i++) {
+          System.out.println("Then we have... \n" +
+              "With " + teamArray.get(i).sumTotalScore() + " points, it's team " + teamArray.get(i).getPlayer1()
+              + " and "
+              + teamArray.get(i).getPlayer2() + "!\n");
+        }
+      }
+    }
 
   }
 }
